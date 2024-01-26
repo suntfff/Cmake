7084fbdf0104059e8a30bf6c7376dbe255c04f89
suntff <157761880+suntfff@users.noreply.github.com>
2024-01-26 18:13:05 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <ctime>
+#include "func.hpp"
+int main()
+{
+	std::vector<std::string>  v;
+	std::ifstream in("output.txt");
+	std::ofstream out("output.txt", std::ios::app);
+	func::number(in, out, v);
+	func::date(out);
+	in.close();
+	out.close();
+	return 0;
+}
