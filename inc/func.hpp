a83d620b51a360dcf78dfd834627d7fcea7b26e2
suntff <157761880+suntfff@users.noreply.github.com>
2024-02-03 12:00:49 +0200

    Update func.hpp

--- inc/func.hpp
+++ inc/func.hpp
@@ -1,4 +1,7 @@
 ﻿#pragma once
+#include <fstream>
+#include <vector>
+#include <string>
 namespace func {
 	void number(std::ifstream& in, std::ofstream& out, std::vector<std::string> v);
 	void date(std::ofstream& out);
